93ed14c62fb38d83fe3e620b7b62ddee6e3ee69f
LastRevengeA3D <58293259+LastRevengeA3D@users.noreply.github.com>
2019-12-05 13:59:51 +0100

    prueba

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+       Debug.Log("hola");
+    }
+}
